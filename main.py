7a21d29c62de519acb16e30fdee3d3fe326d3f6b
劳维旻 <genyez@163.com>
2022-09-26 20:44:23 +0800

    init run

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        file = QFile("ashby.ui")
+        file.open(QFile.ReadOnly)
+        file.close()
+
+        self.ui = QUiLoader().load(file)
+        self.ui.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
