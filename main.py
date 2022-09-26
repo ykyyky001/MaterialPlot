86bcd9c5c8250a238fd58e527cdc9ce05fb4f993
劳维旻 <genyez@163.com>
2022-09-26 23:24:40 +0800

    add: OpenCSV template

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 from PySide2.QtCore import QFile, QRectF, QPointF
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene
+from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QFileDialog
 from PySide2.QtGui import QBrush, QPen, QColor, QFont
+import csv
 
 
 class MainWindow(QMainWindow):
@@ -22,9 +23,21 @@ class MainWindow(QMainWindow):
         self.pen = QPen(QColor(0,0,0))
         self.brush = QBrush(QColor(100,0,0, 100))
         self.brush2 = QBrush(QColor(100,100,0, 100))
+        self.currentData = []
 
     def connectSignals(self):
         self.ui.pushButton.clicked.connect(self.onClick)
+        self.ui.actionOpen_CSV.triggered.connect(self.onOpenCSV)
+
+    def onOpenCSV(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open CSV", filter="CSV Files (*.csv)")
+        if filename:
+            self.currentData.clear()
+            with open(filename, "r") as f:
+                csvrows = csv.DictReader(f)
+                for index, row in enumerate(csvrows):
+                    self.currentData.append(row)
+            print(self.currentData)
 
     def onClick(self):
         print("clicked!")
