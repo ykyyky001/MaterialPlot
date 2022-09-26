19cfe780fbd91cab32dc14f32c0b8c2d8de2790a
劳维旻 <genyez@163.com>
2022-09-26 21:07:42 +0800

    add: fatcow test

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
-from PySide2.QtCore import QFile
+from PySide2.QtCore import QFile, QRectF, QPointF
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsScene
+from PySide2.QtGui import QBrush, QPen, QColor, QFont
 
 
 class MainWindow(QMainWindow):
@@ -12,7 +13,32 @@ class MainWindow(QMainWindow):
         file.close()
 
         self.ui = QUiLoader().load(file)
+        self.connectSignals()
         self.ui.show()
+
+        self.myScene = QGraphicsScene()
+        self.ui.graphicsView.setScene(self.myScene)
+
+        self.pen = QPen(QColor(0,0,0))
+        self.brush = QBrush(QColor(100,0,0, 100))
+        self.brush2 = QBrush(QColor(100,100,0, 100))
+
+    def connectSignals(self):
+        self.ui.pushButton.clicked.connect(self.onClick)
+
+    def onClick(self):
+        print("clicked!")
+        self.onGenerate()
+
+    def onGenerate(self):
+        ecl = self.myScene.addEllipse(QRectF(0, 0, 200, 100), self.pen, self.brush)
+        text = self.myScene.addText("FatCow", QFont("Arial", 20, 2))
+
+        ecl.setPos(QPointF(100, 100))
+        text.setPos(QPointF(140, 170))
+
+        ecl.setRotation(45)
+        text.setRotation(45)
 
 
 if __name__ == '__main__':
